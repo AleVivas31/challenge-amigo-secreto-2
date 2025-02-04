bc1e7ddb7120717921379eef93f04dff98a2669d
Alejandra Vivas <alejandravivasb31@gmail.com>
2025-02-03 21:48:02 -0300

    Agregar a archivo app.js

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ let nombreAmigos = [];
 
 function agregarAmigo(){
     let registroNombre = document.getElementById('amigo').value;
-    alert (registroNombre);
+
+        if (registroNombre === '') {
+            alert("Por favor, inserte un nombre");
+            return;
+        } 
+        
+    console.log (registroNombre);
+    return;
 
 }
 
+    
 
+
+    
+    
+
+
+
+
+
+
+    
 
